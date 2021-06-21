dc2f13d956a7f695de678ee27db7269cf8efd5ce
zorro griss <zorrogriss@gmail.com>
2021-06-20 20:50:30 -0500

    Checking if building process works, all work ok

---
+++ My_Workspace/host/000TestProject/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: 20 jun. 2021
+ *      Author: zorro
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello world\n");
+
+	printf("Press 'Enter' to extir this application\n");
+
+	getchar();
+
+	return 0;
+}
